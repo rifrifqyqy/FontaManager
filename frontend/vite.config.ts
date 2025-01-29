d82e4d00c29aca95a9fdca7deffd70fac1ea6a23
rifrifqyqy <rifqyh22@gmail.com>
2025-01-30 03:45:06 +0700

    build:

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server:{
+  server: {
     proxy: {
-      '/api': { 
-        target: 'https://fonta-manager.vercel.app/', // Gunakan target lokal ini saat pengembangan
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+      "/api": "https://fonta-manager.vercel.app/",
+    },
+  },
 });
